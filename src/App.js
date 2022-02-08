ae0ac01a728a86689af786f9887f7d38b8e6d107
tapia1013 <manbearpig1013@gmail.com>
2022-02-08 14:05:12 -0800

    added notfound, about and home links and component pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 
@@ -10,7 +13,12 @@ function App() {
         <Navbar />
 
         <main className='container mx-auto px-3 pb-12'>
-          Content
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
         </main>
 
         <Footer />
