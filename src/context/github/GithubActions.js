6f15bebd2064b827577f602143733accfdc59d39
tapia1013 <manbearpig1013@gmail.com>
2022-02-09 15:39:35 -0800

    added axios and some refactored headers

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,6 +1,13 @@
+import axios from 'axios';
+
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
+// instance of axios so we wont have to repeat the Headers
+const github = axios.create({
+  baseURL: GITHUB_URL,
+  headers: { Authorization: `token ${GITHUB_TOKEN}` }
+})
 
 // Get Search results
 export const searchUsers = async (text) => {
@@ -8,50 +15,22 @@ export const searchUsers = async (text) => {
     q: text
   })
 
-  const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    }
-  })
+  const response = await github.get(`/search/users?${params}`)
 
-  const { items } = await response.json()
-
-  return items
+  return response.data.items
 }
 
+// Get User and Repos
+export const getUserAndRepos = async (login) => {
+  // make multiple request with Promise.all
+  const [user, repos] = await Promise.all([
+    github.get(`/users/${login}`),
+    github.get(`/users/${login}/repos`)
+  ])
 
-
-
-// Get Single User
-export const getUser = async (login) => {
-  const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    }
-  })
-
-  if (response.status === 404) {
-    window.location = '/notfound'
-  } else {
-    const data = await response.json()
-
-    return data
+  return {
+    user: user.data,
+    repos: repos.data
   }
 }
 
-// Get User Repos
-export const getUserRepos = async (login) => {
-  const params = new URLSearchParams({
-    sort: 'created',
-    per_page: 10
-  })
-
-  const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    }
-  })
-  const data = await response.json()
-
-  return data
-}
