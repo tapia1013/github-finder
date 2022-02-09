92066e25b8558bb30a540d353a94b93ba9de81ee
tapia1013 <manbearpig1013@gmail.com>
2022-02-08 17:28:10 -0800

    search user functionality works also changed fetchuser to searchUsers

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -14,21 +14,27 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
-  // Get initial users(testing purposes)
-  const fetchUsers = async () => {
-    setLoading()
 
-    const response = await fetch(`${GITHUB_URL}/users`, {
+
+  // Get Search results
+  const searchUsers = async (text) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      q: text
+    })
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       }
     })
 
-    const data = await response.json()
+    const { items } = await response.json()
 
     dispatch({
       type: 'GET_USERS',
-      payload: data
+      payload: items,
     })
 
   }
@@ -40,7 +46,7 @@ export const GithubProvider = ({ children }) => {
     <GithubContext.Provider value={{
       users: state.users,
       loading: state.loading,
-      fetchUsers,
+      searchUsers,
     }}>
       {children}
     </GithubContext.Provider>
